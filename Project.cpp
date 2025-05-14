34510d45ff9322bb02bd9e9971de58c008005783
mohamed <MohamedHosnyDev@gmail.com>
2025-05-14 10:44:58 +0200

    Building the students system

--- Project.cpp
+++ Project.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
+
+class Students
+{
+    private:
+        string StudentsDataKey ;
+        int StudentsDataValue ;
+        multimap<string,int> StudentsData;
+    public:
+        void SetStudentDataItem()
+        {
+            cout << "Please enter the student name and his grade" <<endl;
+            cin >> StudentsDataKey ;
+            cin >> StudentsDataValue ; 
+            StudentsData.insert({StudentsDataKey , StudentsDataValue});
+        }
+
+        int GetStudentGrade()
+        {
+            cout << "Please enter the student name" <<endl;
+            cin >> StudentsDataKey;
+            multimap<string,int> :: iterator Item ;
+            Item = StudentsData.find(StudentsDataKey);
+            return Item->second;
+        }
+
+        void PrintAllStudentsData()
+        {
+            multimap<string,int> :: iterator Item ;
+            for (Item = StudentsData.begin(); Item != StudentsData.end(); ++Item)
+            {
+                cout << "Student Name: " << Item->first << " Grade: " << Item->second << endl;
+            }
+        }
+
+};
 
 int main()
 {
     
 }
+
