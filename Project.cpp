20de094c30affe82cbd874e29d1172ddfc4ab2e2
mohamed <MohamedHosnyDev@gmail.com>
2025-05-14 11:51:18 +0200

    Building the managers system

--- Project.cpp
+++ Project.cpp
@@ -1,8 +1,58 @@
 #include <iostream>
-
+#include <string>
+#include <map>
 using namespace std;
+
+class Managers
+{
+private:
+    string ManagersDataKey;
+    float ManagersDataValue;
+    multimap<string, float> ManagersData;
+public:
+    void SetManagerDataItem()
+    {
+        cout << "Please enter the Manager name and their ownership percentage" << endl;
+        cin >> ManagersDataKey;
+        cin >> ManagersDataValue;
+        ManagersData.insert({ManagersDataKey, ManagersDataValue});
+    }
+
+    float GetManagerOwnershipPercentage()
+    {
+        cout << "Please enter the Manager name" << endl;
+        cin >> ManagersDataKey;
+        multimap<string, float>::iterator Item;
+        Item = ManagersData.find(ManagersDataKey);
+        if (Item == ManagersData.end())
+        {
+            cout << "Manager not found!" << endl;
+            return 0.0f;
+        }
+        return Item->second;
+    }
+
+    void PrintAllManagersData()
+    {
+        multimap<string, float>::iterator Item;
+        for (Item = ManagersData.begin(); Item != ManagersData.end(); ++Item)
+        {
+            cout << "Manager Name: " << Item->first << " Ownership Percentage: " << Item->second << "%" << endl;
+        }
+    }
+};
+
 
 int main()
 {
-    
+    Managers ManagersSystem;
+
+    for (int i = 0; i < 4; i++)
+    {
+        ManagersSystem.SetManagerDataItem();
+    }
+
+    cout << "The ownership percentage of the manager is: " << ManagersSystem.GetManagerOwnershipPercentage() << "%" << endl;
+
+    ManagersSystem.PrintAllManagersData();
 }
