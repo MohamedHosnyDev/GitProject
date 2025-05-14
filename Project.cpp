5b2d01f763b14ce436ee6bfa07917d6a593aa881
mohamed <MohamedHosnyDev@gmail.com>
2025-05-14 11:05:25 +0200

    Building the teachers system

--- Project.cpp
+++ Project.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
+
+class Teachers
+{
+    private:
+        string TeachersDataKey ;
+        int TeachersDataValue ;
+        multimap<string,int> TeachersData;
+    public:
+        void SetTeacherDataItem()
+        {
+            cout << "Please enter the Teacher name and his age" <<endl;
+            cin >> TeachersDataKey ;
+            cin >> TeachersDataValue ; 
+            TeachersData.insert({TeachersDataKey , TeachersDataValue});
+        }
+
+        int GetTeacherAge()
+        {
+            cout << "Please enter the Teacher name" <<endl;
+            cin >> TeachersDataKey;
+            multimap<string,int> :: iterator Item ;
+            Item = TeachersData.find(TeachersDataKey);
+            return Item->second;
+        }
+
+        void PrintAllTeachersData()
+        {
+            multimap<string,int> :: iterator Item ;
+            for (Item = TeachersData.begin(); Item != TeachersData.end(); ++Item)
+            {
+                cout << "Teacher Name: " << Item->first << " Age: " << Item->second << endl;
+            }
+        }
+
+};
 
 int main()
 {
